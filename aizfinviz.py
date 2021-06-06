9f6a2fec8b23feda16cc12e50c634f1e9b53c2a8
aizwellenstan <aizwellenstan@gmail.com>
2021-06-06 17:37:35 +0900

    lts

--- aizfinviz.py
+++ aizfinviz.py
@@ -3,6 +3,8 @@ import requests
 import urllib3
 from lxml import html
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import pandas as pd
+import sys
 
 STOCK_URL = "https://finviz.com/quote.ashx"
 NEWS_URL = "https://finviz.com/news.ashx"
@@ -49,11 +51,20 @@ def get_page(ticker):
     )
 
 def get_insider(ticker):
-  get_page(ticker)
-  page_parsed = STOCK_PAGE[ticker]
-  table = page_parsed.cssselect('table[class="body-table"]')[0]
-  headers = table[0].xpath("td//text()")
-  data = [dict(zip(headers, row.xpath("td//text()"))) for row in table[1:]]
-  return data
+    try:
+        data = []
+        get_page(ticker)
+        page_parsed = STOCK_PAGE[ticker]
+        res = page_parsed.cssselect('table[class="body-table"]')
+        if(len(res) > 0):
+            table = res[0]
+            headers = table[0].xpath("td//text()")
+            data = [dict(zip(headers, row.xpath("td//text()"))) for row in table[1:]]
+        return data
+    except Exception as e:
+        print("Error on line {}".format(sys.exc_info()[-1].tb_lineno))
+        print(e)
 
-get_insider('AAPL')
+insider = get_insider('AAPL')
+df = pd.DataFrame(insider)
+df.to_csv('./csv/insider.csv')
